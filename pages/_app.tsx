28bbb566582c443bfa0fe182f54221ac210210c3
George <gex7777@gmail.com>
2022-11-18 23:38:35 +0530

    wrap konsta to next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { App as Konsta } from "konsta/react";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Konsta>
+      <Component {...pageProps} />
+    </Konsta>
+  );
 }
