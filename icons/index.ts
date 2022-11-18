c4ed132b67a4598cbab1dbca83e4689539f61cd9
George <gex7777@gmail.com>
2022-11-18 23:36:11 +0530

    add icons

---
+++ icons/index.ts
@@ -0,0 +1,6 @@
+// add  viewBox="0 0 42 42" fill="currentColor before using svgs
+export { default as Bitcoin } from "./img/bitcoin.svg";
+export { default as MyAccount } from "./img/myAccount.svg";
+export { default as Reward } from "./img/rewards.svg";
+export { default as GoLive } from "./img/golive.svg";
+export { default as Withdraw } from "./img/withdraws.svg";
