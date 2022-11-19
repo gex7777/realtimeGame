57bccbfb3ea565916b4725d05c60e7f9add47cad
George <gex7777@gmail.com>
2022-11-19 21:17:03 +0530

    add carosel

--- pages/golive.tsx
+++ pages/golive.tsx
@@ -1,10 +1,24 @@
+import { Card, Carousel, Dropdown } from "flowbite-react";
+import { Button, Link, Navbar } from "konsta/react";
 import React from "react";
+import GameSelectCard from "../components/GameSelectCard";
 import Layout from "./../components/Layout";
 
 const Golive = () => {
   return (
     <Layout>
-      <div>Golive</div>
+      <Navbar title="Choose game"></Navbar>
+      <Carousel className="h-[80%]" slide={false}>
+        <div className="flex h-full items-center justify-center bg-md-light-surface-2 dark:bg-md-dark-surface-2 dark:text-white">
+          <GameSelectCard rate={1} name="1 dollar table" />
+        </div>
+        <div className="flex h-full items-center justify-center bg-md-light-surface-2 dark:bg-md-dark-surface-2 dark:text-white">
+          <GameSelectCard rate={5} name="5 dollar table" />
+        </div>
+        <div className="flex h-full items-center justify-center bg-md-light-surface-2 dark:bg-md-dark-surface-2  dark:text-white">
+          <GameSelectCard rate={10} name="10 dollar table" />
+        </div>
+      </Carousel>
     </Layout>
   );
 };
