d73d5955b3788fb29d365220ecd469900e0bd603
George <gex7777@gmail.com>
2022-11-20 23:24:22 +0530

    reverse traingle layout for cards

--- pages/golive.tsx
+++ pages/golive.tsx
@@ -8,17 +8,18 @@ const Golive = () => {
   return (
     <Layout>
       <Navbar title="Choose game"></Navbar>
-      <Carousel className="h-[80%]" slide={false}>
-        <div className="flex h-full items-center justify-center bg-md-light-surface-2 dark:bg-md-dark-surface-2 dark:text-white">
-          <GameSelectCard rate={1} name="1 dollar table" />
-        </div>
-        <div className="flex h-full items-center justify-center bg-md-light-surface-2 dark:bg-md-dark-surface-2 dark:text-white">
-          <GameSelectCard rate={5} name="5 dollar table" />
-        </div>
-        <div className="flex h-full items-center justify-center bg-md-light-surface-2 dark:bg-md-dark-surface-2  dark:text-white">
-          <GameSelectCard rate={10} name="10 dollar table" />
-        </div>
-      </Carousel>
+      <div className="flex justify-center">
+        <GameSelectCard rate={5} name="hi" />
+        <GameSelectCard rate={5} name="hi" />
+        <GameSelectCard rate={5} name="hi" />
+      </div>
+      <div className="flex justify-center">
+        <GameSelectCard rate={5} name="hi" />
+        <GameSelectCard rate={5} name="hi" />
+      </div>
+      <div className="flex justify-center">
+        <GameSelectCard rate={5} name="hi" />
+      </div>
     </Layout>
   );
 };
