b8894ee10ec345b58d5960f00d11f06c049921f3
George <gex7777@gmail.com>
2022-11-20 23:23:33 +0530

    test content

--- components/GameSelectCard.tsx
+++ components/GameSelectCard.tsx
@@ -6,35 +6,19 @@ interface Iprops {
 }
 const GameSelectCard: FC<Iprops> = ({ rate, name }) => {
   return (
-    <div className="flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col h-5/6 w-5/6">
-      <div className="flex flex-col justify-between h-full p-4">
-        <div className="flex flex-col  items-center ">
-          <h5 className="pt-[40%] text-6xl font-medium text-gray-900 dark:text-white">
-            ${rate}
-          </h5>
-          <span className="text-sm text-gray-500 dark:text-gray-400">
-            {name}
-          </span>
-          <a
-            href="#"
-            className="mt-[20%] inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-6xl font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Play
-          </a>
-        </div>
-        <div className=" flex justify-between ">
-          <a
-            href="#"
-            className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Add friend
-          </a>
-          <a
-            href="#"
-            className="inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-          >
-            Message
-          </a>
+    <div className="card  bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img
+          src="https://placeimg.com/400/225/arch"
+          alt="Shoes"
+          className="rounded-xl"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
