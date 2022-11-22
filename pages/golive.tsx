64ef8df02665ae31040acb90002e72adb544d6f0
George <gex7777@gmail.com>
2022-11-22 22:25:10 +0530

    change a bit somthing

--- pages/golive.tsx
+++ pages/golive.tsx
@@ -10,12 +10,12 @@ const Golive = () => {
       <Navbar className="" title="Choose game"></Navbar>
 
       <div className="mx-4  gap-4  grid grid-cols-2 place-content-center">
+        <GameSelectCard rate={1} name="hi" />
         <GameSelectCard rate={5} name="hi" />
-        <GameSelectCard rate={5} name="hi" />
-        <GameSelectCard rate={5} name="hi" />
-        <GameSelectCard rate={5} name="hi" />
-        <GameSelectCard rate={5} name="hi" />
-        <GameSelectCard rate={5} name="hi" />
+        <GameSelectCard rate={10} name="hi" />
+        <GameSelectCard rate={20} name="hi" />
+        <GameSelectCard rate={100} name="hi" />
+        <GameSelectCard rate={1000} name="hi" />
       </div>
     </Layout>
   );
