9a4ed4772f4c7a3d1f481c4aafcfae4e94266894
George <gex7777@gmail.com>
2022-11-24 23:09:30 +0530

    rename routes

--- pages/go-live.tsx
+++ pages/go-live.tsx
@@ -2,13 +2,11 @@ import { Card, Carousel, Dropdown } from "flowbite-react";
 import { Button, Link, Navbar } from "konsta/react";
 import React from "react";
 import GameSelectCard from "../components/GameSelectCard";
-import Layout from "./../components/Layout";
+import Layout from "../components/Layout";
 
 const Golive = () => {
   return (
     <Layout>
-      <Navbar className="" title="Choose game"></Navbar>
-
       <div className="mx-4  gap-4  grid grid-cols-2 place-content-center">
         <GameSelectCard rate={1} name="hi" />
         <GameSelectCard rate={5} name="hi" />
