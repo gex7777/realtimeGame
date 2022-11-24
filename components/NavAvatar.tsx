f078a2034297fb2593bbe48ea12abad1df93c241
George <gex7777@gmail.com>
2022-11-24 23:05:46 +0530

    test avatar component

---
+++ components/NavAvatar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavAvatar = () => {
+  return (
+    <div className="avatar">
+      <div className="w-10 rounded-xl">
+        <img src="https://placeimg.com/192/192/people" />
+      </div>
+    </div>
+  );
+};
+
+export default NavAvatar;
