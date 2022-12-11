6e261670dc0c2ad291614ece20f7f488b486feba
George <gex7777@gmail.com>
2022-12-11 23:11:39 +0530

    fix layout

--- pages/bitcoins.tsx
+++ pages/bitcoins.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import Layout from "./../components/Layout";
 
 const Bitcoins = () => {
-  return (
-    <Layout>
-      <div>Bitcoins</div>
-    </Layout>
-  );
+  return <div>Bitcoins</div>;
 };
 
 export default Bitcoins;
