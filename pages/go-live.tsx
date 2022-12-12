40ed6486da8cb1200812986c2de27e55edbc5d37
George <gex7777@gmail.com>
2022-12-12 21:26:51 +0530

    make changes to gameselectmenu

--- pages/go-live.tsx
+++ pages/go-live.tsx
@@ -3,16 +3,36 @@ import { Button, Link, Navbar } from "konsta/react";
 import React from "react";
 import GameSelectCard from "../components/GameSelectCard";
 import Layout from "../components/Layout";
-
+import oneDollar from "../public/1-12-12-2022.png";
+import fiveDollar from "../public/5-12-12-2022.png";
+import tenDollar from "../public/10-12-12-2022.png";
+import twentyDollar from "../public/20-12-12-2022.png";
+import hundredDollar from "../public/100-12-12-2022.png";
+import thousandDollar from "../public/1000-12-12-2022.png";
 const Golive = () => {
   return (
-    <div className="  gap-2  grid grid-cols-2 place-content-center">
-      <GameSelectCard rate={1} name="hi" />
-      <GameSelectCard rate={5} name="hi" />
-      <GameSelectCard rate={10} name="hi" />
-      <GameSelectCard rate={20} name="hi" />
-      <GameSelectCard rate={100} name="hi" />
-      <GameSelectCard rate={1000} name="hi" />
+    <div className="w-full  gap-2  grid grid-cols-2 place-items-center place-content-stretch">
+      <GameSelectCard rate={1} src={oneDollar} name="sample" userCount={890} />
+      <GameSelectCard rate={5} src={fiveDollar} name="sample" userCount={786} />
+      <GameSelectCard rate={10} src={tenDollar} name="sample" userCount={343} />
+      <GameSelectCard
+        rate={20}
+        src={twentyDollar}
+        name="sample"
+        userCount={4435}
+      />
+      <GameSelectCard
+        rate={100}
+        src={hundredDollar}
+        name="hi"
+        userCount={574}
+      />
+      <GameSelectCard
+        rate={1000}
+        src={thousandDollar}
+        name="hi"
+        userCount={3453}
+      />
     </div>
   );
 };
