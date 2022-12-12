1b457830a46c2c07579f1d564ba7cb4aa94decf3
George <gex7777@gmail.com>
2022-12-12 21:25:41 +0530

    add more props to gameCard

--- components/GameSelectCard.tsx
+++ components/GameSelectCard.tsx
@@ -1,25 +1,32 @@
 import { Card, Dropdown } from "flowbite-react";
+import Image, { StaticImageData } from "next/image";
+import { ImUsers } from "react-icons/im";
 import React, { FC } from "react";
 interface Iprops {
   rate: number;
   name: string;
+  src: StaticImageData;
+  userCount: number;
 }
-const GameSelectCard: FC<Iprops> = ({ rate, name }) => {
+const GameSelectCard: FC<Iprops> = ({ rate, name, src, userCount }) => {
   return (
     <div className="card w-44 sm:w-auto  bg-base-100 shadow-xl">
-      <figure className=" ">
-        <img
-          src="https://placeimg.com/400/225/arch"
-          alt="Shoes"
-          className="rounded-xl"
-        />
+      <figure className="mt-8 pt-2">
+        <Image alt={name} height={100} width={100} src={src}></Image>
       </figure>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">${rate}</h2>
-
         <div className="card-actions">
           <button className="btn btn-primary">Play</button>
         </div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-title">Live users</div>
+        <div className="stat-value flex">{userCount}</div>
+      </div>
+
+      <div className="btn btn-md rounded-b-lg btn-secondary ">
+        Invite Friends
       </div>
     </div>
   );
