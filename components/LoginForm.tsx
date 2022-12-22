28183a0976ec24e3ef16a44c41b67780d8592aba
George <gex7777@gmail.com>
2022-12-23 00:24:39 +0530

    create login form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,83 +1,35 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import React from "react";
 
 const LoginForm = () => {
+  const supabase = useSupabaseClient();
+
   return (
-    <form className="m-3 space-y-4 md:space-y-6" action="#">
-      <div>
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your email
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="name@company.com"
-          required={true}
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="••••••••"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required={true}
-        />
-      </div>
-      <div className="flex items-center justify-between">
-        <div className="flex items-start">
-          <div className="flex items-center h-5">
-            <input
-              id="remember"
-              aria-describedby="remember"
-              type="checkbox"
-              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-              required={true}
-            />
-          </div>
-          <div className="ml-3 text-sm">
-            <label
-              htmlFor="remember"
-              className="text-gray-500 dark:text-gray-300"
-            >
-              Remember me
-            </label>
-          </div>
-        </div>
-        <a
-          href="#"
-          className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-        >
-          todo Forgot password?
-        </a>
-      </div>
-      <button
-        type="submit"
-        className="w-full btn btn-primary text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-      >
-        Sign in
-      </button>
-      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-        todo Don’t have an account yet?{" "}
-        <a
-          href="#"
-          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-        >
-          todo Sign up
-        </a>
-      </p>
-    </form>
+    <div className="m-3">
+      <Auth
+        supabaseClient={supabase}
+        appearance={{
+          theme: ThemeSupa,
+          className: {
+            anchor: "link link-primary",
+            button: "btn btn-primary",
+            loader: "loading",
+            input:
+              "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+          },
+          variables: {
+            default: {
+              colors: {
+                brand: "#007aff",
+                brandAccent: "#3a99ff",
+              },
+            },
+          },
+        }}
+        providers={["google", "facebook", "twitter"]}
+      />
+    </div>
   );
 };
 
